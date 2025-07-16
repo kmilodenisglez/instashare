28b106295483f0adb43d723383e9ef469c35a41a
kmilo <kmilo.denis.glez@yandex.com>
2025-07-16 12:39:15 -0400

    add origin to app

--- instashare/app/main.py
+++ instashare/app/main.py
@@ -28,7 +28,7 @@ app.add_middleware(SessionMiddleware, secret_key=os.getenv("SESSION_SECRET_KEY",
 # CORS for development
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
