08094d1fb1e11ffdaa56107384236c3422b91a35
kmilo <kmilo.denis.glez@yandex.com>
2025-07-16 12:07:23 -0400

    add fastapi metadata

--- instashare/app/main.py
+++ instashare/app/main.py
@@ -10,7 +10,17 @@ import os
 # load environment variables from .env
 load_dotenv()
 
-app = FastAPI()
+app = FastAPI(
+    title='Instashare DFS API',
+    redoc_url=None,
+    version='1.0.0',
+    description='Instashare DFS API',
+    contact={
+        'name': 'Instashare',
+        'url': '',
+        'email': 'kmilo.denis.glez@gmail.com'
+    }
+)
 
 # session middleware
 app.add_middleware(SessionMiddleware, secret_key=os.getenv("SESSION_SECRET_KEY", "default_secret_key"))
