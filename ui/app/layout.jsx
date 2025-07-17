f0fd6eadc9edee3334ee365036642ae1caad5440
kmilo <kmilo.denis.glez@yandex.com>
2025-07-17 09:12:06 -0400

    add title

--- ui/app/layout.jsx
+++ ui/app/layout.jsx
@@ -14,6 +14,7 @@ export default function RootLayout({ children }) {
         <html lang="en">
             <head>
                 <link rel="icon" href="/favicon.svg" sizes="any" />
+                <title>Instashare DFS</title>
             </head>
             <body className="antialiased text-white bg-blue-900">
                 <div className="flex flex-col min-h-screen px-6 bg-noise sm:px-12">
