db51bb2607caf876940bcf82568a60be2d6bc1e6
kmilo <kmilo.denis.glez@yandex.com>
2025-07-17 08:53:49 -0400

    use new login button

--- ui/components/header.jsx
+++ ui/components/header.jsx
@@ -2,15 +2,11 @@ import Image from 'next/image';
 import Link from 'next/link';
 import netlifyLogo from 'public/netlify-logo.svg';
 import githubLogo from 'public/images/github-mark-white.svg';
-import LoginButton from "./LoginButton";
+import AuthButton from "./AuthButton";
 
 const navItems = [
     { linkText: 'Home', href: '/' },
     { linkText: 'Revalidation', href: '/revalidation' },
-    { linkText: 'Image CDN', href: '/image-cdn' },
-    { linkText: 'Edge Function', href: '/edge' },
-    { linkText: 'Blobs', href: '/blobs' },
-    { linkText: 'Classics', href: '/classics' }
 ];
 
 export function Header() {
@@ -38,7 +34,7 @@ export function Header() {
             >
                 <Image src={githubLogo} alt="GitHub logo" className="w-7" />
             </Link>
-            <LoginButton />
+            <AuthButton />
         </nav>
     );
 }
