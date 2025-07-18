14e1f466c06c4f6b05224caf84acbd7ab46c651b
kmilo <kmilo.denis.glez@yandex.com>
2025-07-18 16:35:41 -0400

    comment navItems var

--- ui/components/header.jsx
+++ ui/components/header.jsx
@@ -6,9 +6,9 @@ import instashareLogo from 'public/instashare-logo.svg';
 import githubLogo from 'public/images/github-mark-white.svg';
 import AuthButton from './AuthButton';
 
-const navItems = [
-    // { linkText: 'Home', href: '/' },
-];
+// const navItems = [
+//     { linkText: 'Home', href: '/' },
+// ];
 
 export function Header() {
     return (
@@ -16,17 +16,17 @@ export function Header() {
             <Link href="/">
                 <Image src={instashareLogo} alt="InstaShare logo" width={200} height={50} />
             </Link>
-            {!!navItems?.length && (
-                <ul className="flex flex-wrap gap-x-4 gap-y-1">
-                    {navItems.map((item, index) => (
-                        <li key={index}>
-                            <Link href={item.href} className="inline-flex px-1.5 py-1 sm:px-3 sm:py-2">
-                                {item.linkText}
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-            )}
+            {/*{!!navItems?.length && (*/}
+            {/*    <ul className="flex flex-wrap gap-x-4 gap-y-1">*/}
+            {/*        {navItems.map((item, index) => (*/}
+            {/*            <li key={index}>*/}
+            {/*                <Link href={item.href} className="inline-flex px-1.5 py-1 sm:px-3 sm:py-2">*/}
+            {/*                    {item.linkText}*/}
+            {/*                </Link>*/}
+            {/*            </li>*/}
+            {/*        ))}*/}
+            {/*    </ul>*/}
+            {/*)}*/}
             <Link
                 href="https://github.com/kmilodenisglez/instashare"
                 target="_blank"
