1f9b0ad0ce79e0fa3da408e281816fec08472101
kmilo <kmilo.denis.glez@yandex.com>
2025-07-17 21:04:57 -0400

    add new origins

--- api/main.py
+++ api/main.py
@@ -36,7 +36,7 @@ app.add_middleware(
 # CORS for development
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://instashare-qba.netlify.app", ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
