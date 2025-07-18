262064fbbdb479b14e2fb81778c18d3982ab2bf1
kmilo <kmilo.denis.glez@yandex.com>
2025-07-18 09:12:40 -0400

    use AuthProvider

--- ui/app/layout.jsx
+++ ui/app/layout.jsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import { Footer } from '../components/footer';
 import { Header } from '../components/header';
+import { AuthProvider } from '../components/AuthContext';
 
 export const metadata = {
     title: {
@@ -56,9 +57,11 @@ export default function RootLayout({ children }) {
         <body className="antialiased text-white bg-blue-900">
         <div className="flex flex-col min-h-screen px-6 bg-noise sm:px-12">
             <div className="flex flex-col w-full max-w-5xl mx-auto grow">
-                <Header />
-                <main className="grow">{children}</main>
-                <Footer />
+                <AuthProvider>
+                    <Header />
+                    <main className="grow">{children}</main>
+                    <Footer />
+                </AuthProvider>
             </div>
         </div>
         </body>
